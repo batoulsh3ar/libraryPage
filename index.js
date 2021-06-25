820348373dc2175d4daac6aa51285e6e6e08058f
unknown <batoulsh3ar@gmail.com>
2021-06-25 20:26:36 +0300

    test4

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ fetch('https://traininghshaar.herokuapp.com/')
 json.forEach(element => {
     
     var y= document.createElement('span');
-    var content=`<div class="card,row" style="width:200px;text-align:center">
-    <img class="card-img-top" src="xxx" alt="Card image" style="vertical-align:middle">
+    var content=`<div class="card,row" style="width:400px;text-align:center;margin-left:150px;">
+    <img class="card-img-top" src="xxx" alt="Card image" style="vertical-align:middle"><br/><br/>
     <div class="card-body,col">
-      <h5 class="card-title">yyy</h4>
-      <p class="card-text">zzz  </p>
-      <a href="nn" class="btn btn-primary">Download</a>
+      <strong><p class="card-title">yyy</p></strong>
+      <strong><p class="card-text">zzz  </p></strong>
+      <a href="nn" class="btn btn-primary" style="width:300px;font-size:30px">Download</a>
     </div>
-  </div><br/><hr>`;
+  </div><br/><p style=" margin-left: -10px;margin-right: -10px ; border: 5px solid rgb(181,101,29)"></p>`;
   var test= content.replace("xxx",element.img);
   test= test.replace("yyy",element.name);
   test= test.replace("zzz",element.author+"<br/>"+element.pages);
   test= test.replace("nn",element.link);
     y.innerHTML=test;
     
-  document.getElementById('aya').appendChild(y);
+  document.getElementById('book').appendChild(y);
     
 }))
